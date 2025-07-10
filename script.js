6ce1da36161f3c04cde98b1ff1f2f00913472458
alinaaa770 <aquach@willamette.edu>
2025-07-10 16:41:09 +0000

    woah

---
+++ script.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css"
+        <script type="text-javacript" src="script.js">
+        <title>
+            Woah
+        </title>
+    </head>
+    <body onclick="woah()">
+    Woah!
+    </body>
+</html>
