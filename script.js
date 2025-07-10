aee9d0e8d4d341d2c1f9aaf4829310d07f3c7200
alinaaa770 <aquach@willamette.edu>
2025-07-10 16:44:36 +0000

    introducry

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" href="style.css"
-        <script type="text-javacript" src="script.js">
-        <title>
-            Woah
-        </title>
-    </head>
-    <body onclick="woah()">
-    Woah!
-    </body>
-</html>
+function hello(){
+
+    alert("hi guys!")
+}
